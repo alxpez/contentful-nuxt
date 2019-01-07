9f6b8d4c56b48530459f7ddbe4562edebe83f138
Alex Perez <alex.perez@heyday.co.nz>
2019-01-07 15:54:27 +1300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,80 @@
+const pkg = require('./package')
+const env = require('./.env')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Environment variables (available via process.env [nodejs] or context object [browser])
+  env: {
+    CTF_SPACE_ID: env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: env.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: env.CTF_PERSON_ID,
+    CTF_BLOG_POST_TYPE_ID: env.CTF_BLOG_POST_TYPE_ID
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
